86c50ccdf1943ac8bb712490e623a544b6404571
BogdanGhinea <bogdanghinea84@gmail.com>
2022-08-16 13:46:08 +0200

    SeedData Class -ensure DB Populated

---
+++ SportsStore/Models/SeedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<StoreDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
