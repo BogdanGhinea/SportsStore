31249941657eb75ca6963a702ffe68ebefbf9a52
BogdanGhinea <bogdanghinea84@gmail.com>
2022-08-17 10:38:22 +0200

    Tag Helper erstellt

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.Routing;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportsStore.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SportsStore.Infrastructure
 {
+    [HtmlTargetElement("div",Attributes = "page-model")]
     public class PageLinkTagHelper : TagHelper
     {
         private IUrlHelperFactory urlHelperFactory;
@@ -14,6 +19,27 @@ namespace SportsStore.Infrastructure
     public PageLinkTagHelper(IUrlHelperFactory helperFactory)
         {
             urlHelperFactory = helperFactory;
+        }
+        [ViewContext]               //unter View Context kann man html manipulieren
+        [HtmlAttributeNotBound]       
+        public ViewContext ViewContext { get; set; }
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+           //wir habe html pages in div  
+            TagBuilder result = new TagBuilder("div");
+            //wir werden  a(ancor)haben  für jedes Page
+            for (int i = 1; i < PageModel.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction,
+                    new { productPage = i });
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+            output.Content.AppendHtml(result.InnerHtml);
         }
     }
 }
